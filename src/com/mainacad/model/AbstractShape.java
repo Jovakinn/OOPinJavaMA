2602ea7c11f20b2718780f2dcecd26399857ba39
38098 <max05012004@gmail.com>
2019-06-27 20:35:18 +0200

    Initial commit

---
+++ src/com/mainacad/model/AbstractShape.java
@@ -0,0 +1,5 @@
+package com.mainacad.model;
+
+public abstract class AbstractShape implements Shape {
+
+}
---
+++ src/com/mainacad/model/Quadrlateral.java
@@ -0,0 +1,19 @@
+package com.mainacad.model;
+
+public class Quadrlateral extends Square {
+    private double base;
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    @Override
+    public double getArea(){
+        return base * getSide();
+    }
+
+}
